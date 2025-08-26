e4dde33fe6240cda6b5f291577e2603d36805735
Ian Brown <bigianb@gmail.com>
2025-08-26 18:03:04 +0100

    tutorial

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
-import { app, BrowserWindow } from 'electron/main'
+import { app, BrowserWindow, ipcMain } from 'electron/main'
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
---
+++ preload.js
@@ -0,0 +1,11 @@
+// Sandboxed renderer preload scripts cannot use ESM
+const { contextBridge, ipcRenderer } = require('electron/renderer');
+
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
+  // we can also expose variables, not just functions
+})
